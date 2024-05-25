7e5f43b0f30b5eff51c8ac63e2ac63502b2fe413
Jefferson Roberto <135037032+jeffin-Dev@users.noreply.github.com>
2024-05-25 10:40:11 -0300

    Commit feito do notebook dia 25/05

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, redirect, url_for, request
-from model import db, Usuario, Agenda, Barbearia, PrecoServico
+from model import db, Usuario, Agenda, Barbearia, PrecoServico, Servico
 from flask_sqlalchemy import SQLAlchemy 
 from flask_login import LoginManager, login_user, UserMixin, login_required, current_user, logout_user
 from scripts.enviar_email_recuperar import enviar_email_recuperar_senha
@@ -8,6 +8,7 @@ from datetime import datetime
 
 
 app = Flask(__name__)
+
 
 app.config['SECRET_KEY'] = 'A123S456D789'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost/bdnaregua'
@@ -20,13 +21,12 @@ login_manager.login_view = 'login' #Rota de login
 login_manager.login_message='Faça Login Ou Cadastre-se Para Acessar' #Mensagem que aparecerá caso você seja obrigado a fazer o login
 login_manager.login_message_category='error'#Categoria do login    
     
+
+
 @login_manager.user_loader
 def load_user(user_id):
     user = Usuario.query.filter_by(cpf=user_id).first()
     return user
-
-
-
 
 
 @app.route('/login')
@@ -87,11 +87,11 @@ def cadastrar():
     #Rota cadastrar, apenas renderiza o template cadastrar.html
     return render_template('cadastrar.html')
 
-# Esta função é uma rota Flask que recebe os dados de cadastro de um formulário HTML via método POST.
+# Esta função é uma rota Flask que recebe os dados de cadastro para cadastrar um CLIENTE 
 @app.route('/coletar-cadastro', methods=['POST'])
 def coletar_cadastro():
     # Obtém os dados do formulário enviado.
-    nomeCompleto = request.form['nomeCompleto']
+    nomeUsuario = request.form['nomeCompleto']
     cpf = request.form['cpf']
     telefone = request.form['telefone']
     email = request.form['email']
@@ -99,7 +99,7 @@ def coletar_cadastro():
     senha2 = request.form['senha2']
 
     # Verifica se algum campo obrigatório está em branco.
-    if (nomeCompleto == '') or (telefone == '') or (email == '') or (senha1 == '') or (senha2 == ''):
+    if (nomeUsuario == '') or (telefone == '') or (email == '') or (senha1 == '') or (senha2 == ''):
         print('Houve algum campo em branco.')
         # Redireciona de volta para a página de cadastro.
         return redirect(url_for('cadastrar'))
@@ -107,11 +107,11 @@ def coletar_cadastro():
         # Verifica se as senhas fornecidas coincidem.
         if str(senha1) == str(senha2):
             # Formata os dados antes de criar um novo usuário.
-            nomeCompleto = nomeCompleto.capitalize()
+            nomeUsuario = nomeUsuario.capitalize()
             email = email.lower()
 
             # Cria um novo objeto de usuário com os dados fornecidos.
-            novo_usuario = Usuario(cpf=cpf, nomeCompleto=nomeCompleto, email=email, telefone=telefone, senha=senha1, barbeiro = False)
+            novo_usuario = Usuario(cpf=cpf, nomeUsuario=nomeUsuario, email=email, telefone=telefone, senha=senha1, barbeiro = False)
             
             # Adiciona o novo usuário ao banco de dados.
             db.session.add(novo_usuario)
@@ -130,49 +130,50 @@ def cadastrar_barbeiro():
     print('Entrei aqui')
     return render_template('cadastro-barbeiro.html')
 
-
+# Esta função é uma rota Flask que recebe os dados de cadastro do barbeiro de um formulário HTML via método POST.
 @app.route('/coletar-cadastro-barbeiro', methods=['POST'])
 def coletar_cadastro_barbeiro():
+    # Verifica se o usuário atual está autenticado
     if current_user.is_authenticated:
-        id_usuario = current_user.idUsuario
+        id_usuario = current_user.idUsuario  # Obtém o ID do usuário atual
         print('O id do user é:', id_usuario)
     
-    nomeBarbearia = request.form['nomeBarbearia']
-    telefone = request.form['telefone']
-    rua = request.form['rua']
-    numero = request.form['numero']
-    bairro = request.form['bairro']
-    cep = request.form['cep']
-    cidade = request.form['cidade']
-    estado = request.form['estado']
-    horaInicio = request.form['horario_abertura_sim']
-    horaFim = request.form['horario_fechamento_sim']
-    barbeiro = True
+    # Coleta os dados do formulário enviados via método POST
+    nomeBarbearia = request.form['nomeBarbearia']  # Nome da barbearia
+    telefone = request.form['telefone']  # Telefone da barbearia
+    rua = request.form['rua']  # Rua do endereço da barbearia
+    numero = request.form['numero']  # Número do endereço da barbearia
+    bairro = request.form['bairro']  # Bairro do endereço da barbearia
+    cep = request.form['cep']  # CEP do endereço da barbearia
+    cidade = request.form['cidade']  # Cidade do endereço da barbearia
+    estado = request.form['estado']  # Estado do endereço da barbearia
+    horaInicio = request.form['horario_abertura_sim']  # Horário de abertura da barbearia
+    horaFim = request.form['horario_fechamento_sim']  # Horário de fechamento da barbearia
+    barbeiro = True  # Define que o usuário é barbeiro
     
+    # Constrói o endereço completo
     endereco = f'{rua}, {numero} - {bairro}, CEP: {cep} - {cidade}, {estado}'
     
-    novoBarbeiro = Barbearia(idUsuario=id_usuario, nomeBarbearia=nomeBarbearia, telefone=telefone, endereco=endereco, horaInicio=horaInicio, horaFim=horaFim)
+    # Cria uma nova instância de 'Barbearia' com os dados coletados
+    novoBarbeiro = Barbearia(nomeBarbearia=nomeBarbearia, telefone=telefone, endereco=endereco, horaInicio=horaInicio, horaFim=horaFim)
     
+    # Adiciona a nova barbearia à sessão do banco de dados e confirma a transação
     db.session.add(novoBarbeiro)
-    
-    usuario = Usuario.query.filter_by(idUsuario=id_usuario).first()
-    if usuario:
-        
-        usuario.barbeiro = True
-
     db.session.commit()
-    print(f'''
-Cadastro feito!!
-Nome: {nomeBarbearia}.
-Endereço: {endereco}.
-Telefone: {telefone}.
-Horario de abertura: {horaInicio}.
-Horario de fechamento: {horaFim}.
-Barberiro? {barbeiro}.
-''')
-    
-    
 
+    # Obter o idBarbearia da barbearia recém-criada
+    id_barbearia = novoBarbeiro.idBarbearia
+
+    # Consulta o banco de dados para encontrar o usuário pelo seu ID
+    usuario = Usuario.query.filter_by(idUsuario=id_usuario).first()
+    
+    if usuario:
+        # Atualiza o usuário para indicar que ele é barbeiro e associa a barbearia recém-criada
+        usuario.barbeiro = True
+        usuario.idBarbearia_fk = id_barbearia
+        db.session.commit()  # Confirma a transação para salvar as alterações
+
+    # Redireciona o usuário para a página 'barber'
     return redirect(url_for('barber'))
     
 
@@ -180,6 +181,7 @@ Barberiro? {barbeiro}.
 def esq_senha():
     return render_template('esqueci_minha_senha.html')
 
+# Esta função é uma rota Flask que recebe os dados de esqueci minha senha.
 @app.route('/coletar_email_recuperar', methods=['POST'])
 def coletar_email_recuperar():
      
@@ -262,70 +264,188 @@ def coletar_nova_senha():
         return redirect(url_for('criar_senha'))
 
 
+# Define a rota '/home-barbearia' e a rota raiz '/' para a função 'barber'
 @app.route('/home-barbearia')
 @app.route('/')
 def barber():
+    # Verifica se o usuário atual está autenticado
     if current_user.is_authenticated:
+        # Converte o valor do campo 'barbeiro' do usuário atual para inteiro
         barbeiro = int(current_user.barbeiro)
-        print(barbeiro, type(barbeiro))
-        return render_template('sistema-homepage.html', barbeiro = barbeiro)
+        print(barbeiro, type(barbeiro))  # Imprime o valor e o tipo de 'barbeiro' para depuração
+        # Renderiza o template 'sistema-homepage.html', passando o valor de 'barbeiro' e a lista de todos os serviços
+        return render_template('sistema-homepage.html', barbeiro=barbeiro, servicos=Servico.query.all())
     else:
-        return render_template('sistema-homepage.html')
+        # Renderiza o template 'sistema-homepage.html', passando apenas a lista de todos os serviços
+        return render_template('sistema-homepage.html', servicos=Servico.query.all())
 
 
-@app.route('/agendamentos/barbeiro')
+# Define a rota '/servico/<nome_servico>' para a função 'servico_escolhido'
+@app.route('/servico/<nome_servico>')
+def servico_escolhido(nome_servico):
+    # Consulta a tabela 'PrecoServico' juntando com 'Servico' para filtrar pelo nome do serviço fornecido
+    preco_servico = PrecoServico.query.join(Servico).filter(Servico.nomeServico == nome_servico).all()
+
+    try:
+        # Inicializa listas para armazenar barbearias, valores dos serviços e IDs dos serviços
+        barbearias = []
+        valores_servicos = []
+        id_servico = []
+        
+        # Itera sobre os registros de 'preco_servico' encontrados
+        for p in preco_servico:
+            if p:
+                id_barbearia = p.idBarbearia  # Obtém o ID da barbearia associado ao preço do serviço
+                barbearia = Barbearia.query.filter_by(idBarbearia=id_barbearia).first()  # Consulta a barbearia pelo ID
+                
+                if barbearia:
+                    barbearias.append(barbearia)  # Adiciona a barbearia à lista
+                    valores_servicos.append(p.PrecoServico)  # Adiciona o valor do serviço à lista
+                    id_servico.append(p.idPrecoServico)  # Adiciona o ID do preço do serviço à lista
+        
+        # Renderiza o template 'sistema-homepage.html' passando todos os serviços, as barbearias, os valores dos serviços e o nome do serviço
+        return render_template('sistema-homepage.html', servicos=Servico.query.all(), barbearias_valores=zip(barbearias, valores_servicos, id_servico), nome_servico=nome_servico)
+    except:
+        # Em caso de exceção, exibe uma mensagem de erro e redireciona para a página 'barber'
+        flash('Nenhum barbeiro encontrado para o serviço escolhido.', 'error')
+        return redirect(url_for('barber'))
+
+
+@app.route('/agendamentos/<nome_barbearia>/<id_servico>')
 @login_required
-def agendamento():
-    return render_template('reservar-horario.html')
+def agendamento(nome_barbearia, id_servico):
+    return render_template('reservar-horario.html', id_servico=id_servico)
 
-@app.route('/coletar-agendamento', methods=['POST'])
-def coletar_agendamento():
-    dataAgendamento = request.form['dataAgendamento']
-    horaAgendamento = request.form['horaAgendamento']
+
+# Define a rota '/coletar-agendamento/<int:id_servico>' para a função 'coletar_agendamento' usando o método POST
+@app.route('/coletar-agendamento/<int:id_servico>', methods=['POST'])
+def coletar_agendamento(id_servico):
+    # Coleta os dados do formulário enviados via método POST
+    dataAgendamento = request.form['dataAgendamento']  # Data do agendamento
+    horaAgendamento = request.form['horaAgendamento']  # Hora do agendamento
     
-    nova_agenda = Agenda(idPrecoServico= 1, idUsuario=1, idStatus=1, dataAtendimento=dataAgendamento, horarioAtendimento=horaAgendamento)
+    # Cria uma nova instância de 'Agenda' com os dados coletados e o ID do serviço fornecido
+    nova_agenda = Agenda(idPrecoServico_fk=id_servico, idUsuario_fk=current_user.idUsuario, idStatus_fk=1, dataAtendimento=dataAgendamento, horarioAtendimento=horaAgendamento)
+    
+    # Adiciona o novo agendamento à sessão do banco de dados e confirma a transação
     db.session.add(nova_agenda)
     db.session.commit()
     
-    print('Você agendou um corte da data:', dataAgendamento, '\nO horario do agendamento é: ', horaAgendamento)
-    return redirect(url_for('agendamento'))
+    # Converte a data do agendamento de string para datetime
+    dataAgendamento_datetime = datetime.strptime(dataAgendamento, '%Y-%m-%d')
+    # Formata a data para o formato brasileiro (dd/mm/yyyy)
+    dataAgendamento_br = dataAgendamento_datetime.strftime('%d/%m/%Y')
+    
+    # Exibe uma mensagem de sucesso com os detalhes do agendamento
+    flash(f"Agendamento Concluido! Foi feito uma reserva na data: {dataAgendamento_br}, ás: {horaAgendamento}.", 'success')
+    
+    # Redireciona o usuário para a página 'reservasCliente'
+    return redirect(url_for('reservasCliente'))
 
 
+# Define a rota '/reservas' para a função 'reservasCliente' e exige que o usuário esteja autenticado para acessar
 @app.route('/reservas')
 @login_required
 def reservasCliente():
+    # Verifica se o usuário atual está autenticado
     if current_user.is_authenticated:
-        id_usuario = current_user.idUsuario
-        reservas = Agenda.query.filter_by(idUsuario=id_usuario).all()
+        id_usuario = current_user.idUsuario  # Obtém o ID do usuário atual
+
+        # Consulta a tabela 'Agenda' para obter todas as reservas feitas pelo usuário atual
+        agendas = Agenda.query.filter_by(idUsuario_fk=id_usuario).all()
         
-        lista_de_reservas = []
-        data_atual = datetime.now().date()
+        lista_de_reservas = []  # Inicializa uma lista para armazenar as reservas futuras
+        data_atual = datetime.now().date()  # Obtém a data atual
     
-        for reserva in reservas:
-            if reserva.dataAtendimento >= data_atual:
-                lista_de_reservas.append(reserva)
+        # Itera sobre as reservas do usuário
+        for agenda in agendas:
+            # Verifica se a data do atendimento é igual ou posterior à data atual
+            if agenda.dataAtendimento >= data_atual:
+                lista_de_reservas.append(agenda)  # Adiciona a reserva à lista de reservas futuras
 
-    return render_template('reservas-clientes.html', reservas=lista_de_reservas)
+    # Renderiza o template 'reservas-clientes.html', passando a lista de reservas futuras
+    return render_template('reservas-clientes.html', agendas=lista_de_reservas)
 
+
+# Define a rota '/agendamentos' para a função 'reservasBarbeiro' e exige que o usuário esteja autenticado para acessar
 @app.route('/agendamentos')
 @login_required
 def reservasBarbeiro():
-    id_barbearia = current_user.barbearia.idBarbearia if current_user.barbearia else None
-    lista_de_agendamentos = []
-    data_atual = datetime.now().date()
+    # Obtém o ID do usuário logado
+    id_usuario_logado = current_user.idUsuario
+    print('id do usuario é:', id_usuario_logado)
+    
+    # Verifica se o usuário é barbeiro
+    if current_user.barbeiro == '1':
+        print("Este usuário é um barbeiro.")
+        id_barbearia_usuario = current_user.idBarbearia_fk  # Obtém o ID da barbearia associada ao usuário
+        print('o id da barbearia é:', id_barbearia_usuario)
 
-    if id_barbearia:
-        
-        # Consulta de agendamentos para a barbearia atual
-        agendamentos = Agenda.query.join(PrecoServico).join(Barbearia).filter(PrecoServico.idBarbearia == id_barbearia).all()
-        
-        for agendamento in agendamentos:
-            if agendamento.dataAtendimento >= data_atual:
-                lista_de_agendamentos.append(agendamento)
-        
+        # Consulta para verificar as agendas do barbeiro associadas à barbearia
+        agenda_usuario = (Agenda.query
+                          .join(PrecoServico, Agenda.idPrecoServico_fk == PrecoServico.idPrecoServico)
+                          .filter(PrecoServico.idBarbearia == id_barbearia_usuario)
+                          .all())
+
+        # Se houver agendas, cria uma lista com essas agendas
+        if agenda_usuario:
+            lista_agendas = []
+            for agenda in agenda_usuario:
+                lista_agendas.append(agenda)
+            
+            # Renderiza o template 'reservas-barbeiro.html', passando a lista de agendas
+            return render_template('reservas-barbeiro.html', agendas=lista_agendas)
+        else:
+            # Se não houver agendas, renderiza o template com uma lista vazia
+            return render_template('reservas-barbeiro.html', agendas=[])
+    else:
+        print("Este usuário não é um barbeiro.")
+        # Redireciona o usuário para a página 'barber' se ele não for barbeiro
+        return redirect(url_for('barber'))
+
     
-    return render_template('reservas-barbeiro.html', agendamentos=lista_de_agendamentos)
+@app.route('/cadastro-servico')
+def cadastrarServicos():
+    return render_template('cadastrar_servico.html', servicos = Servico.query.all())
     
+
+# Define a função que coleta os serviços do barbeiro e salva no banco
+@app.route('/coletar-servicos-barbeiro', methods=['POST'])
+def coletarServicosBarbeiro():
+    # Obter todos os checkboxes marcados
+    servicos_selecionados = request.form.getlist('servico_valor')
+    
+    servico_valor_pairs = []  # Inicializa uma lista para armazenar pares de serviço e valor
+
+    # Processar os dados do formulário
+    for servico_id in servicos_selecionados:
+        # Obter o valor do input associado a este serviço
+        valor_key = f'valorServico_{servico_id}'
+        valor = request.form.get(valor_key, '')
+        
+        if valor:  # Certifique-se de que há um valor associado
+            # Cria uma nova instância de 'PrecoServico' com os dados coletados
+            preco_servico_barbeiro = PrecoServico(dataInicio=datetime.now().strftime('%Y-%m-%d'), 
+                                                  idBarbearia=current_user.idBarbearia_fk, 
+                                                  idServico=servico_id, 
+                                                  PrecoServico=valor)
+            
+            # Adiciona o novo preço de serviço à sessão do banco de dados e confirma a transação
+            db.session.add(preco_servico_barbeiro)
+            db.session.commit()
+            flash('Serviços cadastrados com sucesso!', 'success')
+        else:
+            # Se algum serviço não tiver valor, exibe uma mensagem de erro e redireciona
+            flash('Coloque valor em todos os serviços selecionados.', 'error')
+            return redirect(url_for('cadastrarServicos'))
+
+    # Printar os serviços e valores (não necessário para a funcionalidade, mas útil para depuração)
+    for servico, valor in servico_valor_pairs:
+        print(f'Serviço: {servico}, Valor: {valor}')
+
+    # Redireciona o usuário para a página 'cadastrarServicos' após concluir o processamento
+    return redirect(url_for('cadastrarServicos'))
+
 
 @app.route('/logout')
 @login_required
--- model.py
+++ model.py
@@ -1,22 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 
+# Inicializa a extensão SQLAlchemy
 db = SQLAlchemy()
 
+# Define a classe 'Usuario', que representa a tabela 'usuario' no banco de dados
 class Usuario(db.Model):
     __tablename__ = 'usuario'
     
-    idUsuario = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nomeUsuario = db.Column(db.String(100))
-    cpf = db.Column(db.String(15), primary_key=True)
-    telefone = db.Column(db.String(20))
-    email = db.Column(db.String(45))
-    senha = db.Column(db.String(12))
-    barbeiro = db.Column(db.Integer)
-    idBarbeiro = db.Column(db.Integer, db.ForeignKey('barbearia.idBarbearia'))
-    
-    
-    barbearia = db.relationship('Barbearia', foreign_keys=[idBarbeiro], backref=db.backref('usuarios', lazy=True))
-    
+    # Define as colunas da tabela 'usuario'
+    idUsuario = db.Column(db.Integer, primary_key=True, autoincrement=True)  # Chave primária com incremento automático
+    nomeUsuario = db.Column(db.String(100))  # Coluna para armazenar o nome do usuário
+    cpf = db.Column(db.String(15), primary_key=True)  # Coluna para armazenar o CPF do usuário, também usado como chave primária
+    telefone = db.Column(db.String(20))  # Coluna para armazenar o telefone do usuário
+    email = db.Column(db.String(45))  # Coluna para armazenar o email do usuário
+    senha = db.Column(db.String(12))  # Coluna para armazenar a senha do usuário
+    barbeiro = db.Column(db.String(1), default='0')  # Coluna para indicar se o usuário é barbeiro (0 ou 1)
+    idBarbearia_fk = db.Column(db.Integer)  # Coluna para armazenar a referência à barbearia
     
     def is_authenticated(self):
         # Lógica para determinar se o usuário está autenticado
@@ -26,78 +25,82 @@ class Usuario(db.Model):
         # Lógica para determinar se o usuário está ativo ou não
         return True  # ou False, dependendo da lógica de ativação
 
-
     def get_id(self):
-        # Lógica para pegar o cpf do usuario
+        # Lógica para pegar o CPF do usuário
         return str(self.cpf)
-    
 
-
+# Define a classe 'Barbearia', que representa a tabela 'barbearia' no banco de dados
 class Barbearia(db.Model):
     __tablename__ = 'barbearia'
 
-    idBarbearia = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    idUsuario = db.Column(db.Integer, db.ForeignKey('usuario.idUsuario'))
-    nomeBarbearia = db.Column(db.String(45))
-    endereco = db.Column(db.String(150))
-    telefone = db.Column(db.String(20))
-    horaInicio = db.Column(db.Time())
-    horaFim = db.Column(db.Time())
-    
-    
-    def get_id(self):
-        # Lógica para pegar o cpf do barbeiro
-        return str(self.id_usuario)    
+    # Define as colunas da tabela 'barbearia'
+    idBarbearia = db.Column(db.Integer, primary_key=True, autoincrement=True)  # Chave primária com incremento automático
+    nomeBarbearia = db.Column(db.String(45))  # Coluna para armazenar o nome da barbearia
+    endereco = db.Column(db.String(150))  # Coluna para armazenar o endereço da barbearia
+    telefone = db.Column(db.String(20))  # Coluna para armazenar o telefone da barbearia
+    horaInicio = db.Column(db.Time())  # Coluna para armazenar a hora de início do funcionamento
+    horaFim = db.Column(db.Time())  # Coluna para armazenar a hora de fim do funcionamento
 
+    def get_id(self):
+        # Lógica para pegar o ID da barbearia
+        return str(self.id_usuario)
+
+# Define a classe 'Agenda', que representa a tabela 'agenda' no banco de dados
 class Agenda(db.Model):
     __tablename__ = 'agenda'
     
-    idReserva = db.Column(db.Integer, primary_key=True)
-    idPrecoServico = db.Column(db.Integer, db.ForeignKey('preco_servico.codPrecoServico'), nullable=False)
-    idUsuario = db.Column(db.Integer, db.ForeignKey('usuario.idUsuario'), nullable=False)
-    idStatus = db.Column(db.Integer, db.ForeignKey('status.idStatus'), nullable=False)
-    dataAtendimento = db.Column(db.Date, nullable=False)
-    horarioAtendimento = db.Column(db.Time, nullable=False)
+    # Define as colunas da tabela 'agenda'
+    idReserva = db.Column(db.Integer, primary_key=True)  # Chave primária
+    idPrecoServico_fk = db.Column(db.Integer, db.ForeignKey('preco_servico.idPrecoServico'), nullable=False)  # Chave estrangeira para 'preco_servico'
+    idUsuario_fk = db.Column(db.Integer, db.ForeignKey('usuario.idUsuario'), nullable=False)  # Chave estrangeira para 'usuario'
+    idStatus_fk = db.Column(db.Integer, db.ForeignKey('status.idStatus'), nullable=False)  # Chave estrangeira para 'status'
+    dataAtendimento = db.Column(db.Date, nullable=False)  # Coluna para armazenar a data do atendimento
+    horarioAtendimento = db.Column(db.Time, nullable=False)  # Coluna para armazenar o horário do atendimento
     
     # Relacionamentos opcionais
-    preco_servico = db.relationship('PrecoServico', backref=db.backref('agendas', lazy=True))
-    usuario = db.relationship('Usuario', backref=db.backref('agendas', lazy=True))
-    status = db.relationship('Status', backref=db.backref('agendas', lazy=True))
+    preco_servico_fk = db.relationship('PrecoServico', backref=db.backref('agendas', lazy=True))  # Relacionamento com 'PrecoServico'
+    usuario_fk = db.relationship('Usuario', backref=db.backref('agendas', lazy=True))  # Relacionamento com 'Usuario'
+    status_fk = db.relationship('Status', backref=db.backref('agendas', lazy=True))  # Relacionamento com 'Status'
     
-    def __init__(self, idPrecoServico, idUsuario, idStatus, dataAtendimento, horarioAtendimento):
-        self.idPrecoServico = idPrecoServico
-        self.idUsuario = idUsuario
-        self.idStatus = idStatus
+    def __init__(self, idPrecoServico_fk, idUsuario_fk, idStatus_fk, dataAtendimento, horarioAtendimento):
+        # Inicializa uma nova instância de 'Agenda'
+        self.idPrecoServico_fk = idPrecoServico_fk
+        self.idUsuario_fk = idUsuario_fk
+        self.idStatus_fk = idStatus_fk
         self.dataAtendimento = dataAtendimento
         self.horarioAtendimento = horarioAtendimento
 
     def __repr__(self):
+        # Representação string da instância 'Agenda'
         return f'<Agenda {self.idReserva}>'
-    
-    
+
+# Define a classe 'Status', que representa a tabela 'status' no banco de dados
 class Status(db.Model):
     __tablename__ = 'status'
     
-    idStatus = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    descricao = db.Column(db.String(50))
-    
+    # Define as colunas da tabela 'status'
+    idStatus = db.Column(db.Integer, primary_key=True, autoincrement=True)  # Chave primária com incremento automático
+    descricao = db.Column(db.String(50))  # Coluna para armazenar a descrição do status
 
+# Define a classe 'PrecoServico', que representa a tabela 'preco_servico' no banco de dados
 class PrecoServico(db.Model):
     __tablename__ = 'preco_servico'
     
-    codPrecoServico = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    dataInicio = db.Column(db.Date, nullable=False)
-    idBarbearia = db.Column(db.Integer, db.ForeignKey('barbearia.idBarbearia'), nullable=False)
-    idServico = db.Column(db.Integer, db.ForeignKey('servico.idServico'), nullable=False)
-    PrecoServico = db.Column(db.Float, nullable=False)
+    # Define as colunas da tabela 'preco_servico'
+    idPrecoServico = db.Column(db.Integer, primary_key=True, autoincrement=True)  # Chave primária com incremento automático
+    dataInicio = db.Column(db.Date, nullable=False)  # Coluna para armazenar a data de início do serviço
+    idBarbearia = db.Column(db.Integer, db.ForeignKey('barbearia.idBarbearia'), nullable=False)  # Chave estrangeira para 'barbearia'
+    idServico = db.Column(db.Integer, db.ForeignKey('servico.idServico'), nullable=False)  # Chave estrangeira para 'servico'
+    PrecoServico = db.Column(db.Float, nullable=False)  # Coluna para armazenar o preço do serviço
     
-    #Relações pela tabela Preco_Servico
-    servico = db.relationship('Servico', backref=db.backref('precos_servico', lazy=True))
-    barbearia = db.relationship('Barbearia', backref=db.backref('precos_servico', lazy=True))
-    
-    
+    # Relacionamentos pela tabela 'PrecoServico'
+    servico = db.relationship('Servico', backref=db.backref('precos_servico', lazy=True))  # Relacionamento com 'Servico'
+    barbearia = db.relationship('Barbearia', backref=db.backref('precos_servico', lazy=True))  # Relacionamento com 'Barbearia'
+
+# Define a classe 'Servico', que representa a tabela 'servico' no banco de dados
 class Servico(db.Model):
     __tablename__ = 'servico'
     
-    idServico = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nomeServico = db.Column(db.String(250), nullable=False)
+    # Define as colunas da tabela 'servico'
+    idServico = db.Column(db.Integer, primary_key=True, autoincrement=True)  # Chave primária com incremento automático
+    nomeServico = db.Column(db.String(250), nullable=False)  # Coluna para armazenar o nome do serviço
